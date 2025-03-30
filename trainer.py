a229ffdd72027dc95e39c4f8ce412fe908722aa7
qfeuilla <quentinfeuillademontixi42@gmail.com>
2025-03-30 14:38:22 +0200

    remove flash attn

--- trainer.py
+++ trainer.py
@@ -467,10 +467,11 @@ def main():
     config = TokenOfPowerConfig(
         model_name="meta-llama/Llama-3.1-8B-Instruct",
         max_length=256,
-        batch_size=2,
+        batch_size=4,
         dataset_path="./llama_3_8b_instruct/dataset.json",
         wandb_entity="ToPMaster",
-        gradient_accumulation_steps=1
+        gradient_accumulation_steps=1,
+        flash_attention=False,
     )
     trainer = TokenOfPowerTrainer(config)
     trainer.train()
