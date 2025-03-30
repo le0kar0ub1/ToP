de8a7e833840a0c43d65e7de395e86d09c7dfa64
qfeuilla <quentinfeuillademontixi42@gmail.com>
2025-03-30 14:07:09 +0200

    feat: less memory

--- trainer.py
+++ trainer.py
@@ -455,11 +455,11 @@ class TokenOfPowerTrainer:
 def main():
     config = TokenOfPowerConfig(
         model_name="meta-llama/Llama-3.1-8B-Instruct",
-        max_length=512,
+        max_length=256,
         batch_size=4,
         dataset_path="./llama_3_8b_instruct/dataset.json",
         wandb_entity="ToPMaster",
-        gradient_accumulation_steps=4
+        gradient_accumulation_steps=2
     )
     trainer = TokenOfPowerTrainer(config)
     trainer.train()
